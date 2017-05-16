8a1b1b238b7790139c2fd9c62b57839f2fca06d6
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2017-05-16 17:28:52 +0200

    usb_example: simplified main to just print a single line

--- nodes/examples/usb/main.c
+++ nodes/examples/usb/main.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#include <stm32f4xx_hal.h>
+
 #include <board_driver/usb/usb.h>
 
 
@@ -9,17 +11,11 @@ int main(void) {
 
 	usb_init();
 
-	// printf("Starting\n\n");
-	// printf("Write something in the terminal and hit Enter:\n");
-
 	while (1) {
-		char *str = "Hellow usb\r\n";
+		char str[] = "Hello usb\r\n";
 
 		usb_transmit(str, sizeof(str)/sizeof(str[0]));
 
-		// USBD_CDC_SetTxBuffer(&USBD_Device, (uint8_t*)str, sizeof(str)/sizeof(str[0]));
-		// char input = getchar();
-		// printf("getchat: %c\n", input);
-		// HAL_Delay(100);
+		HAL_Delay(500);
 	}
 }
