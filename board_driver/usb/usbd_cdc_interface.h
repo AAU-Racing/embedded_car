9dfefd6925a97b14c694b4030109e6165c6330e3
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2017-05-16 18:13:21 +0200

    usb: uart: moved defines for uart3 DMA from usb driver to uart.h

--- board_driver/usb/usbd_cdc_interface.h
+++ board_driver/usb/usbd_cdc_interface.h
@@ -53,13 +53,6 @@
 
 #include "../uart.h"
 
-
-/* Definition for USARTx's DMA: used for transmitting data over Tx pin */
-#define USARTx_TX_DMA_CHANNEL            DMA_CHANNEL_4
-#define USARTx_TX_DMA_STREAM             DMA1_Stream3
-#define USARTx_DMA_TX_IRQHandler         DMA1_Stream3_IRQHandler
-#define USARTx_DMA_TX_IRQn               DMA1_Stream3_IRQn
-
 extern USBD_CDC_ItfTypeDef  USBD_CDC_fops;
 
 /* Exported macro ------------------------------------------------------------*/
