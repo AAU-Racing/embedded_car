37ea25f388f8f42a0fff9398480aa01bf6e2f9af
Jakob Østergaard Jensen <jakob.jensen.91@gmail.com>
2017-10-03 17:51:16 +0200

    shield_drivers/traction_control/water_temp: Outcommented what looks to be unfinnished code coursing errors

--- shield_drivers/traction_control/water_temp.c
+++ shield_drivers/traction_control/water_temp.c
@@ -11,6 +11,10 @@ HAL_StatusTypeDef init_water_temp() {
 	ADCx1_CHANNEL_GPIO_CLK_ENABLE();
 	ADCx2_CHANNEL_GPIO_CLK_ENABLE();
 
+/*
+	The following code course errors.
+	It looks incomplete. Get it fixed!
+*//*
 	init_analog_pins(ADCx1_CHANNEL_GPIO_PORT, ADCx1_CHANNEL_PIN);
 	init_analog_pins(ADCx2_CHANNEL_GPIO_PORT, ADCx2_CHANNEL_PIN);
 
@@ -21,14 +25,17 @@ HAL_StatusTypeDef init_water_temp() {
 	if (init_adc_channel(ADCx2_CHANNEL, channel_rank + 1) != HAL_OK) {
 		return HAL_ERROR;
 	}
+*/
 
 	return HAL_OK;
 }
 
 int read_water_in() {
-    return read_adc_value(channel_rank[0]);
+	//return read_adc_value(channel_rank[0]);
+	return 0;
 }
 
 int read_water_out() {
-    return read_adc_value(channel_rank[1]);
+	//return read_adc_value(channel_rank[1]);
+	return 0;
 }
