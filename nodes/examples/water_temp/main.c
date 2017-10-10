efb7d54ca3b67391455238bbd0d6c3c8c4c326f4
Frederik Mathiesen <frederik_mathies@hotmail.com>
2017-10-10 09:19:34 +0200

    Update ADC

--- nodes/examples/water_temp/main.c
+++ nodes/examples/water_temp/main.c
@@ -12,18 +12,24 @@ int main(void) {
 	uart_init();
 	printf("uart init complete\n");
 
-	init_adc();
+	HAL_Delay(100);
+
+	init_adc(2);
 	printf("adc init complete\n");
 
 	init_water_temp();
 	printf("water sensor init complete\n");
 
+	HAL_Delay(10);
+
 	start_adc();
 	printf("adc started\n");
+	HAL_Delay(10);
 
 	uint16_t i = 0;
 
 	while (1) {
+		HAL_Delay(2);
 		i++;
 		int in = read_water_in();
 		int out = read_water_out();
