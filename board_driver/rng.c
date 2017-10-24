81cbb68c8c37b5fa144284008398fb219178d26b
Frederik Mathiesen <frederik_mathies@hotmail.com>
2017-10-24 18:59:53 +0200

    Clean RNG

--- board_driver/rng.c
+++ board_driver/rng.c
@@ -6,14 +6,10 @@
 void rng_init()
 {
 	//Enable random number clock
-	__IO uint32_t tmpreg; 
     SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_RNGEN);
-    /* Delay after an RCC peripheral clock enabling */ 
-    tmpreg = READ_BIT(RCC->AHB2ENR, RCC_AHB2ENR_RNGEN);
-    (void)tmpreg;
 
     //Enable random number generator on Chip
-	RNG->CR = 1 << 2;
+	SET(RNG->CR, RNG_CR_RNGEN);
 }
 
 //Includes min, but excludes max
