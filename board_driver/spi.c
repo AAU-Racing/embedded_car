bbe7df5f6a8832951db6367494898dd4c674816c
Anne Ejsing <anneejsing77@gmail.com>
2017-10-31 19:26:50 +0100

    First commit. Bunch of stuff. Dsent work.

--- board_driver/spi.c
+++ board_driver/spi.c
@@ -1,4 +1,94 @@
-#include <stm32f4xx_hal.h>
+#include <stm32f407xx.h>
+
+#include "spi.h"
+#define WRITE_REG_MASK(REG, MASK, VAL) WRITE_REG(REG, (READ_REG(REG) & (~(MASK))) | ((VAL) & (MASK)))
+
+#define TX_TIMEOUT_MS	(1000)
+#define BAUDRATEPRESCALER_8 0x010
+#define DIRECTION_2LINES 0x00
+#define PHASE_2EDGE 0x01
+#define POLARITY_HIGH 0x01
+#define CRCCALCULATION_DISABLED 0x00
+#define POLYNOMIAL_RESET 7
+#define DATASIZE_8BIT 0x00
+#define FIRSTBIT_MSB 0x00
+#define NSS_SOFT 0x01
+#define TIMODE_DISABLED 0x00
+#define MODE_MASTER 0x01
+#define MODE_AF_PP 0x02
+#define GPIO_NOPULL 0x0
+
+
+SPI_TypeDef *SPIHandle = SPI1;
+GPIO_TypeDef *GPIOHandle = GPIOA;
+
+
+SPIx_SCK_GPIO_CLK_ENABLE();
+SPIx_MOSI_GPIO_CLK_ENABLE();
+SPIx_CLK_ENABLE();
+
+void GPIO_Init(void)
+{
+			WRITE_REG_MASK(GPIOA->MODER, GPIO_MODER_MODE0_Msk, MODE_AF_PP); //mode
+			WRITE_REG_MASK(GPIOA->PUPDR, GPIO_PUPDR_PUPD0_Msk, GPIO_NOPULL); //pull
+			WRITE_REG_MASK(GPIOA->OSPEEDR, GPIO_OSPEEDR_OSPEED0_Msk, ); //speed
+			WRITE_REG_MASK(GPIOA->, , ); //pin
+			WRITE_REG_MASK(GPIOA->AFR[2], GPIO_AFRL_AFSEL0_Msk, ); //alternate
+}
+
+
+// Configure SPI SCK
+/*GPIO_InitTypeDef gpio = {
+		.Pin       = SPIx_SCK_PIN,
+		.Mode      = GPIO_MODE_AF_PP,
+		.Pull      = GPIO_NOPULL,
+		.Speed     = GPIO_SPEED_FAST,
+		.Alternate = SPIx_SCK_AF,
+};
+HAL_GPIO_Init(SPIx_SCK_GPIO_PORT, &gpio);
+
+	// Configure SPI MOSI
+	gpio.Pin       = SPIx_MOSI_PIN;
+	gpio.Alternate = SPIx_MOSI_AF;
+	gpio.Pull      = GPIO_NOPULL;
+	HAL_GPIO_Init(SPIx_MOSI_GPIO_PORT, &gpio);
+}*/
+
+void HAL_SPI_MspDeInit(SPI_HandleTypeDef *hspi)
+{
+	(void) hspi;
+
+	SPIx_FORCE_RESET();
+	SPIx_RELEASE_RESET();
+
+	HAL_GPIO_DeInit(SPIx_SCK_GPIO_PORT, SPIx_SCK_PIN);
+	HAL_GPIO_DeInit(SPIx_MOSI_GPIO_PORT, SPIx_MOSI_PIN);
+}
+
+
+
+void SPI_init(void)
+{
+		WRITE_REG_MASK(SPI1->CR1, SPI_CR1_BR_Msk, BAUDRATEPRESCALER_8);
+		WRITE_REG_MASK(SPI1->CR1, SPI_CR1_BIDIMODE_Msk , DIRECTION_2LINES);
+		WRITE_REG_MASK(SPI1->CR1, SPI_CR1_CPHA_Msk, PHASE_2EDGE);
+		WRITE_REG_MASK(SPI1->CR1, SPI_CR1_CPOL_Msk , POLARITY_HIGH);
+		WRITE_REG_MASK(SPI1->CR1, SPI_CR1_CRCEN_Msk, CRCCALCULATION_DISABLED);
+		WRITE_REG_MASK(SPI1->CRCPR, SPI_CRCPR_CRCPOLY_Msk, POLYNOMIAL_RESET);
+		WRITE_REG_MASK(SPI1->CR1, SPI_CR1_DFF_Msk, DATASIZE_8BIT);
+		WRITE_REG_MASK(SPI1->CR1, SPI_CR1_LSBFIRST_Msk, FIRSTBIT_MSB);
+		WRITE_REG_MASK(SPI1->CR1, SPI_CR1_SSM_Msk, NSS_SOFT);
+		WRITE_REG_MASK(SPI1->CR2, SPI_CR1_FRF_Msk, TIMODE_DISABLED);
+		WRITE_REG_MASK(SPI1->CR1, SPI_CR1_MSTR_Msk, MODE_MASTER);
+}
+
+
+
+
+
+
+
+/*#include <stm32f4xx_hal.h>
 
 #include "spi.h"
 
@@ -63,4 +153,4 @@ void SPI_init(void) {
 
 HAL_StatusTypeDef SPI_transmit(void *tx, uint32_t size) {
 	return HAL_SPI_Transmit(&spi_handle, tx, size, TX_TIMEOUT_MS);
-}
+}*/
--- board_driver/spi.h
+++ board_driver/spi.h
@@ -4,21 +4,21 @@
 #include <stm32f4xx_hal.h>
 
 #define SPIx                               SPI1
-#define SPIx_CLK_ENABLE()                  __HAL_RCC_SPI1_CLK_ENABLE()
-#define SPIx_FORCE_RESET()				 	__HAL_RCC_SPI1_FORCE_RESET()
-#define SPIx_RELEASE_RESET()				__HAL_RCC_SPI1_RELEASE_RESET()
+#define SPIx_CLK_ENABLE()                  SET_BIT(RCC->APB2ENR, RCC_APB2ENR_SPI1EN)
+#define SPIx_FORCE_RESET()				 	       (RCC->APB2RSTR |= (RCC_APB2RSTR_SPI1RST))
+#define SPIx_RELEASE_RESET()				       (RCC->APB2RSTR &= ~(RCC_APB2RSTR_SPI1RST))
 
 #define SPIx_SCK_AF                        GPIO_AF5_SPI1
 #define SPIx_SCK_GPIO_PORT                 GPIOA
-#define SPIx_SCK_PIN                       GPIO_PIN_5
-#define SPIx_SCK_GPIO_CLK_ENABLE()         __HAL_RCC_GPIOA_CLK_ENABLE()
-#define SPIx_SCK_GPIO_CLK_DISABLE()        __HAL_RCC_GPIOA_CLK_DISABLE()
+#define SPIx_SCK_PIN                       ((uint16_t)0x0020)
+#define SPIx_SCK_GPIO_CLK_ENABLE()         SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOAEN)
+#define SPIx_SCK_GPIO_CLK_DISABLE()        (RCC->AHB1ENER &= ~(RCC_AHB1ENR_GPIOAEN))
 
 #define SPIx_MOSI_AF                  		GPIO_AF5_SPI1
 #define SPIx_MOSI_GPIO_PORT           		GPIOA
-#define SPIx_MOSI_PIN						GPIO_PIN_7
-#define SPIx_MOSI_GPIO_CLK_ENABLE()   		__HAL_RCC_GPIOA_CLK_ENABLE()
-#define SPIx_MOSI_GPIO_CLK_DISABLE()  		__HAL_RCC_GPIOA_CLK_DISABLE()
+#define SPIx_MOSI_PIN						          ((uint16_t)0x0080)
+#define SPIx_MOSI_GPIO_CLK_ENABLE()   		SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOAEN)
+#define SPIx_MOSI_GPIO_CLK_DISABLE()  		(RCC->AHB1ENER &= ~(RCC_AHB1ENR_GPIOAEN))
 
 
 void SPI_init(void);
