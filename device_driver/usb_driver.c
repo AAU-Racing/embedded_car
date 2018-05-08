1eabdaa8f634c488ed1fea93d01a569b40a4a2e9
PB16 <peterbugtrup97@gmail.com>
2018-05-08 09:45:41 -0700

    added initial files for usb driver

---
+++ device_driver/usb_driver.c
@@ -0,0 +1,11 @@
+#include "usb_driver.h"
+#include "board_driver/bootloader_protocol.h"
+#include "board_driver/usb/usb.h"
+
+int usb_driver_receive_packet(Packet* packet) {
+
+}
+
+int usb_driver_transmit_packet(Packet packet) {
+	
+}
---
+++ device_driver/usb_driver.h
@@ -0,0 +1,9 @@
+#include "board_driver/bootloader_protocol.h"
+
+#ifndef USB_DRIVER_H
+#define USB_DRIVER_H
+
+int usb_driver_receive_packet(Packet* packet);
+int usb_driver_transmit_packet(Packet packet);
+
+#endif
