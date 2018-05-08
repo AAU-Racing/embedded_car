661b6da88e9de4bc5d6c486a0090794bb3219dab
PB16 <peterbugtrup97@gmail.com>
2018-05-08 09:48:27 -0700

    added initial files for can modulator

---
+++ device_driver/can_modulator.c
@@ -0,0 +1,11 @@
+#include "can_modulator.h"
+#include "board_driver/bootloader_protocol.h"
+#include "board_driver/can.h"
+
+int can_driver_receive_packet(Packet* packet) {
+
+}
+
+int can_driver_transmit_packet(Packet packet) {
+	
+}
---
+++ device_driver/can_modulator.h
@@ -0,0 +1,9 @@
+#include "board_driver/bootloader_protocol.h"
+
+#ifndef CAN_DRIVER_H
+#define CAN_DRIVER_H
+
+int can_driver_receive_packet(Packet* packet);
+int can_driver_transmit_packet(Packet packet);
+
+#endif
