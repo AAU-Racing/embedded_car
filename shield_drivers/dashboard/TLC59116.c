933f340cca64418a7b7506a2d29c904ba405ac06
Frederik Mathiesen <frederik_mathies@hotmail.com>
2018-07-27 14:59:19 +0200

    Fix LED driver

--- shield_drivers/dashboard/TLC59116.c
+++ shield_drivers/dashboard/TLC59116.c
@@ -1,10 +1,9 @@
 #include <stm32f4xx_hal.h>
 
 #include <board_driver/i2c.h>
+#include <board_driver/can.h>
 
 #include "TLC59116.h"
-
-#define ARR_SIZE(a) (sizeof(a)/sizeof(a[0]))
 
 static bool china_led = false;
 
@@ -26,54 +25,52 @@ enum registers {
 	EFLAG1, EFLAG2,
 };
 
-// The hardware selectable pins from the datasheet
-#define A3	0
-#define A2	0
-#define A1	0
-#define A0	0
-
-#define ADDR ((1 << 7) | (1 << 6) | (0 << 5) | (A3 << 4) | (A2 << 3) | (A1 << 2) | (A0 << 1)) // Not used
+#define ADDR(A3, A2, A1, A0) ((1 << 7) | (1 << 6) | (0 << 5) | (A3 << 4) | (A2 << 3) | (A1 << 2) | (A0 << 1) | 0) // Not used
 
 // Warning because we shift here you cannot just iterate on the address
 enum address {
-	ADDR1 = 0x60 << 1, // Shifted once to make room for the RW bit
-	ADDR2 = 0x61 << 1,
-	ADDR3 = 0x62 << 1,
-	ADDR4 = 0x63 << 1,
+	ADDR1 = ADDR(0,0,0,0),
+	ADDR2 = ADDR(0,0,0,1),
+	ADDR3 = ADDR(0,0,1,0),
+	ADDR4 = ADDR(0,0,1,1),
+	ADDR5 = ADDR(0,1,0,0),
 };
 
-#define controller_lut(x) ((enum address[]){ADDR1,ADDR2,ADDR3,ADDR4}[(x)])
+static enum address controller_lut(uint8_t x) {
+	return ADDR1 + x * 2;
+}
 
-#define N_LEDS_PR_CONTROLLER	(5)
+#define N_LEDS_PR_CONTROLLER 5
 
 /*
 #define R_ADDR	(ADDR | (1 << 0))
 #define W_ADDR	(ADDR | (0 << 0))
 */
 
-
 static void write_cmd(enum address addr, enum registers reg, uint8_t value) {
 	uint8_t p[] = {(uint8_t)reg, value};
-	if (i2c_master_transmit_DMA(addr, p, ARR_SIZE(p)) != 0) {
+
+	if (i2c_master_transmit_DMA(addr, p, 2) != 0) {
 		while(1); // TODO remove
 	}
+	HAL_Delay(1);
+}
+
+static void write_rgb(enum address addr, enum registers reg, uint8_t r, uint8_t g, uint8_t b) {
+	uint8_t p[] = {(uint8_t)reg | (5 << 5), r, g, b};
+
+	if (i2c_master_transmit_DMA(addr, p, 4) != 0) {
+		while(1); // TODO remove
+	}
+	HAL_Delay(1);
 }
 
 static void set_rgb(enum address addr, unsigned led, unsigned r, unsigned g, unsigned b) {
-	enum registers led_lookup[] = {
-		PWM0, PWM3, PWM6,
-		PWM9, PWM12, PWM15,
+	enum registers lookup[] = {
+		PWM0, PWM3, PWM6, PWM9, PWM12,
 	};
 
-	const enum registers pwm_reg = led_lookup[led]; //PWM0 + (led*3);
-
-	const enum registers reg_b = pwm_reg + 0;
-	const enum registers reg_g = pwm_reg + 1;
-	const enum registers reg_r = pwm_reg + 2;
-
-	write_cmd(addr, reg_r, china_led ? g : r);
-	write_cmd(addr, reg_g, china_led ? r : g);
-	write_cmd(addr, reg_b, b);
+	write_rgb(addr, lookup[led], r, g, b);
 }
 
 static void init_reset_pin(void) {
@@ -94,8 +91,8 @@ static void init_controller(enum address ctrl) {
 	write_cmd(ctrl, MODE1, 0x00);
 	write_cmd(ctrl, MODE2, 0x00);
 
-	for (enum registers led_reg = LEDOUT0; led_reg <= LEDOUT3; led_reg++) {
-		write_cmd(ctrl, led_reg, 0xFF);
+	for (enum registers ledout = LEDOUT0; ledout <= LEDOUT3; ledout++) {
+		write_cmd(ctrl, ledout, 0xAA);
 	}
 
 	for (enum registers pwm_reg = PWM0; pwm_reg <= PWM15; pwm_reg++) {
@@ -105,7 +102,9 @@ static void init_controller(enum address ctrl) {
 }
 
 void set_led(unsigned led, unsigned r, unsigned g, unsigned b) {
-	set_rgb(controller_lut((led % 20) / N_LEDS_PR_CONTROLLER), led % N_LEDS_PR_CONTROLLER, r, g, b);
+	enum address addr = controller_lut((led % 25) / N_LEDS_PR_CONTROLLER);
+
+	set_rgb(addr, led % N_LEDS_PR_CONTROLLER, r, g, b);
 }
 
 int led_driver_init(bool china) {
@@ -122,6 +121,7 @@ int led_driver_init(bool china) {
 	init_controller(ADDR2);
 	init_controller(ADDR3);
 	init_controller(ADDR4);
+	init_controller(ADDR5);
 
 	return 0;
 }
