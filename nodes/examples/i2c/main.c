b5128858c3a5d10931fbe33a756cee07143d825e
Frederik Mathiesen <frederik_mathies@hotmail.com>
2018-07-27 14:59:33 +0200

    Change nodes

--- nodes/examples/i2c/main.c
+++ nodes/examples/i2c/main.c
@@ -6,18 +6,32 @@
 
 #include <board_driver/uart.h>
 #include <board_driver/i2c_no_hal.h>
+#include <board_driver/can.h>
 
 int main(void) {
-	// Init uart and crc
-	uart_init();
+	// uart_init();
 	printf("UART init complete\n");
 	HAL_Delay(100);
 
+	if (CAN_Init(CAN_PD0) != CAN_OK) {
+		printf("CAN init error\n");
+	}
+
+	if (CAN_Start() != CAN_OK){
+		printf("CAN start error\n");
+	}
+	else {
+		printf("CAN started\n");
+	}
+
 	i2c_init();
 	printf("i2c init complete\n");
+	CAN_Send(0x7fe, (uint8_t[]) { 2 }, 1);
 
 	for (uint32_t i = 0; ; i++) {
-		i2c_master_transmit(0x1234, (uint8_t*) &i, 4);
+		CAN_Send(0x7fe, (uint8_t[]) { 3 }, 1);
+	 	i2c_master_transmit(17, (uint32_t[]) { 1 }, 4);
+		CAN_Send(0x7fe, (uint8_t[]) { 4 }, 1);
 		printf("Transmit complete\n");
 
 		HAL_Delay(100);
--- nodes/examples/led/main.c
+++ nodes/examples/led/main.c
@@ -5,20 +5,24 @@
 #include <stdint.h>
 
 #include <shield_drivers/dashboard/rpm.h>
+#include <board_driver/can.h>
 
 int main(void) {
-	led_driver_init(true);
+	led_driver_init(false);
+	CAN_Init(CAN_PD0);
+	HAL_Delay(10);
+	CAN_Start();
 
 	while(1) {
-		for (uint8_t color = 0; color < 1; color++) {
-			for (uint8_t led = 0; led < 20; led++) {
-				uint8_t red = (color == 0) * 0xF;
-				uint8_t green = (color == 1) * 0xF;
-				uint8_t blue = (color == 2) * 0xF;
+		for (uint8_t color = 0; color < 3; color++) {
+			for (uint8_t led = 0; led < 25; led++) {
+				uint8_t red = (color == 0) * 0xFF;
+				uint8_t green = (color == 1) * 0xFF;
+				uint8_t blue = (color == 2) * 0xFF;
 
 				set_led(led, red, green, blue);
 
-				HAL_Delay(200);
+				HAL_Delay(25);
 			}
 		}
 	}
