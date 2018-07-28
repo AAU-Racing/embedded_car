9743b934d7a0897096a1e171054364cf333d2f3c
Frederik Mathiesen <frederik_mathies@hotmail.com>
2018-07-28 09:21:04 +0200

    Add software debounce for dashboard and steering wheel buttons

--- shield_drivers/dashboard/buttons.h
+++ shield_drivers/dashboard/buttons.h
@@ -11,48 +11,38 @@ typedef enum {
 	DASHBOARD_BUTTON4,
 	DASHBOARD_BUTTON5,
 	DASHBOARD_BUTTON6,
+	DASHBOARD_BUTTON7,
+	DASHBOARD_BUTTON8,
 
 	DASHBOARD_BUTTONn // number of buttons
 } dashboard_button;
 
-#define BUTTON1_PIN                    PIN_8
-#define BUTTON1_GPIO_PORT              GPIOE
+#define BUTTON1_PIN                    PIN_14
+#define BUTTON1_GPIO_PORT              GPIOD
 
-#define BUTTON2_PIN                    PIN_1
-#define BUTTON2_GPIO_PORT              GPIOB
+#define BUTTON2_PIN                    PIN_15
+#define BUTTON2_GPIO_PORT              GPIOD
 
-#define BUTTON3_PIN                    PIN_12
-#define BUTTON3_GPIO_PORT              GPIOE
+#define BUTTON3_PIN                    PIN_2
+#define BUTTON3_GPIO_PORT              GPIOD
 
-#define BUTTON4_PIN                    PIN_10
-#define BUTTON4_GPIO_PORT              GPIOE
+#define BUTTON4_PIN                    PIN_3
+#define BUTTON4_GPIO_PORT              GPIOD
 
-#define BUTTON5_PIN                    PIN_14
-#define BUTTON5_GPIO_PORT              GPIOE
+#define BUTTON5_PIN                    PIN_4
+#define BUTTON5_GPIO_PORT              GPIOD
 
-#define BUTTON6_PIN                    PIN_7
-#define BUTTON6_GPIO_PORT              GPIOE
+#define BUTTON6_PIN                    PIN_5
+#define BUTTON6_GPIO_PORT              GPIOD
 
-#define BUTTON_PORT ((GPIO_TypeDef* const []) { \
-	BUTTON1_GPIO_PORT, \
-	BUTTON2_GPIO_PORT, \
-	BUTTON3_GPIO_PORT, \
-	BUTTON4_GPIO_PORT, \
-	BUTTON5_GPIO_PORT, \
-	BUTTON6_GPIO_PORT, \
-})
+#define BUTTON7_PIN                    PIN_6
+#define BUTTON7_GPIO_PORT              GPIOD
 
-#define BUTTON_PIN ((const uint16_t const []) { \
-	BUTTON1_PIN, \
-	BUTTON2_PIN, \
-	BUTTON3_PIN, \
-	BUTTON4_PIN, \
-	BUTTON5_PIN, \
-	BUTTON6_PIN, \
-})
+#define BUTTON8_PIN                    PIN_0
+#define BUTTON8_GPIO_PORT              GPIOE
 
 void dashboard_buttons_init(void);
-bool dashboard_button_get_state(dashboard_button b, bool *state);
+bool dashboard_button_get_state(dashboard_button b);
 bool is_triggered(dashboard_button b);
 
 #endif /* DASHBOARD_BUTTONS_H */
