d0170e946d50fe5307933d0a185f529fbe9c3160
Frederik Mathiesen <frederik_mathies@hotmail.com>
2019-06-18 17:58:33 +0200

    Change node to test UART telemetry

--- nodes/examples/uart/main.c
+++ nodes/examples/uart/main.c
@@ -11,6 +11,14 @@ int main(void) {
 	HAL_Delay(10);
 	debug_uart_init(DEV_DEBUG_UART);
 	printf("Starting\n\n");
+
+	uart_init(DEV_TELEMETRY_UART);
+	printf("Done starting the telemetry\n");
+
+	char* hello = "Hello\n";
+	uart_send_buf(DEV_TELEMETRY_UART, hello, 6);
+	printf("Done sending byte\n");
+
 	printf("Write something in the terminal and hit Enter:\n");
 
 	while (1) {
